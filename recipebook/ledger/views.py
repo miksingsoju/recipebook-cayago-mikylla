88f417b5c3481d899479429d18c28dd9688eda63
miksingsoju <161931661+miksingsoju@users.noreply.github.com>
2025-02-24 19:56:34 +0800

    Added ctx and logic for the views

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -2,11 +2,81 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from django.template import loader
 
+ctx = {
+    "recipes": [
+        {
+            "name": "Recipe 1",
+            "ingredients": [
+                {
+                    "name": "tomato",
+                    "quantity": "3pcs"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1kg"
+                },
+                {
+                    "name": "water",
+                    "quantity": "1L"
+                },
+                {
+                    "name": "sinigang mix",
+                    "quantity": "1 packet"
+                }
+            ],
+            "link": "/recipe/1"
+        },
+        {
+            "name": "Recipe 2",
+            "ingredients": [
+                {
+                    "name": "garlic",
+                    "quantity": "1 head"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "vinegar",
+                    "quantity": "1/2cup"
+                },
+                {
+                    "name": "water",
+                    "quanity": "1 cup"
+                },
+                {
+                    "name": "salt",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "whole black peppers",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1 kilo"
+                }
+            ],
+            "link": "/recipe/2"
+        }
+    ]
+}
+
 # Create your views here.
-def index(request):
-    template = loader.get_template('recipe_list.html')
-    return HttpResponse(template.render())
 def recipe_list(request):
-    template = loader.get_template('recipe_list.html')
-    return HttpResponse(template.render())
+    return render(request,'recipe_list.html',ctx)
+
+def recipe(request,num=1):
+    if num is 1:
+        recipe = ctx["recipes"][0]
+    elif num is 2:
+        recipe = ctx["recipes"][1]
+
+    return render(request,'recipe.html',{"recipe":recipe})
+
 
