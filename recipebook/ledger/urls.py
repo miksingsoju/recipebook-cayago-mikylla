f9719e618007dc528204366ea0b285388d4843df
miksingsoju <161931661+miksingsoju@users.noreply.github.com>
2025-02-24 13:27:25 +0800

    Add template

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import index, recipe_list
 
 urlpatterns = [
 path('', index, name='index'),
+path('recipe/list', recipe_list, name='recipe list'),
+
 ]
 # This might be needed, depending
+app_name = "ledger"
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from django.template import loader
 
 # Create your views here.
+def index(request):
+    template = loader.get_template('recipe_list.html')
+    return HttpResponse(template.render())
+def recipe_list(request):
+    template = loader.get_template('recipe_list.html')
+    return HttpResponse(template.render())
+
