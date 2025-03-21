25cb70b7d1b6d9a6884a2a9f5947c246c5762f1a
miksingsoju <161931661+miksingsoju@users.noreply.github.com>
2025-03-21 17:55:54 +0800

    Added Admin for Profile

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,6 +1,9 @@
 # app/admin.py
 from django.contrib import admin
-from .models import Recipe, Ingredient, RecipeIngredient
+from .models import Recipe, Ingredient, RecipeIngredient, Profile
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
 
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
@@ -16,3 +19,14 @@ class RecipeIngredientAdmin(admin.ModelAdmin):
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = "author"
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInline,]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
