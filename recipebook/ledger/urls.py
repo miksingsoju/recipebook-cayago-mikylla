eaf078341348440b07b65ca47369a58c1b62a1fe
miksingsoju <161931661+miksingsoju@users.noreply.github.com>
2025-03-21 17:54:35 +0800

    Updated URLs in Ledger

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from .views import recipe_list, recipe_detail
 urlpatterns = [
 path('', recipe_list, name='recipe-list'),
-path('recipes/list', recipe_list, name='recipe-list'),
-
+path('recipes/list/', recipe_list, name='recipe-list'),
 path('recipe/<int:num>/', recipe_detail,name='recipe-detail'),
 ]
 # This might be needed, depending
