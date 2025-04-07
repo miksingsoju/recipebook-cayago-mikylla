1e60d80059b1525308443fb02b3d35c951e5d219
miksingsoju <161931661+miksingsoju@users.noreply.github.com>
2025-04-07 23:07:41 +0800

    Added these so that you can see the images

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "ledger", "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -126,3 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Added settings for redirecting the user after login is made
 LOGIN_REDIRECT_URL = "/recipes/list"  # Redirect to index
 LOGOUT_REDIRECT_URL = "/accountslogin/"  # Redirect to login after logout
+
+#Handles loading of media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- recipebook/recipebook/urls.py
+++ recipebook/recipebook/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,8 +27,9 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')), 
     path('accounts', include('django.contrib.auth.urls')),
     path('', include('ledger.urls',namespace="ledger")),
-
-
 ]
 
 app_name = "ledger"
+
+if settings.DEBUG:
+   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
